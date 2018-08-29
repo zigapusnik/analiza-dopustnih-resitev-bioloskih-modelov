c15c35b9ae4af930fb5c899b395a0640c7c852b8
zigapusnik <ziga.pusnik@gmail.com>
2018-08-29 20:25:35 +0200

    Update modelSolver.py

--- modelSolver.py
+++ modelSolver.py
@@ -1,3 +1,19 @@
+#Analiza dopustnih resitev bioloskih modelov
+#Copyright (C) 2018  Ziga Pusnik
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation version 3 of the License.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import random
 import math
 import numpy as np
